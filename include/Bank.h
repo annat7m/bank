93f42ead3e7fc31e2b8a0944d475dfc7537bc5b2
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-03-05 04:00:51 +0000

    headers halfway done

--- include/Bank.h
+++ include/Bank.h
@@ -6,3 +6,22 @@
 // Assignment:  Assignment 3 - Bank Accounts
 // Purpose:     Practice Object Oriented Design Skills
 //***************************************************************************
+
+#include <vector>
+#include "../include/Account.h"
+
+#pragma once
+
+class Bank : public Account {
+
+	public:
+		Bank ();
+		virtual ~Bank();
+
+		void addAccount (Account& account); // might be virtual
+		void applyMonthlyUpdates();					// might be virtual
+
+	private:
+		std::vector<Account> mAccounts;
+
+};
