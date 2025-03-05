66cbfc36f444d1a2d54bff0896afe708a5b59455
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-03-05 04:00:51 +0000

    headers halfway done

--- include/Account.h
+++ include/Account.h
@@ -6,3 +6,29 @@
 // Assignment:  Assignment 3 - Bank Accounts
 // Purpose:     Practice Object Oriented Design Skills
 //***************************************************************************
+
+#include <vector>
+#include "../include/Transaction.h"
+
+#pragma once
+
+class Account {
+
+	public:
+		Account (int number, long long balance, double interestRate);
+		virtual ~Account();
+
+		virtual void deposit (long long amount) = 0;
+		virtual void withdraw (long long amount) = 0;
+
+		virtual void chargeMonthlyFee () = 0;
+
+		virtual void accrueInterest ();
+
+	private:
+		int mNumber;
+		long long mBalance;
+		double mInterestRate;
+		// std::vector<Transaction> mTransactions;
+
+};
--- include/CheckingAccount.h
+++ include/CheckingAccount.h
@@ -6,3 +6,26 @@
 // Assignment:  Assignment 3 - Bank Accounts
 // Purpose:     Practice Object Oriented Design Skills
 //***************************************************************************
+
+#include <vector>
+#include "../include/Account.h"
+
+#pragma once
+
+class CheckingAccount : public Account {
+
+	public:
+		CheckingAccount (int accNumber, long long accBalance, double accInterestRate,
+			long long minBalance, long long minBalanceFee);
+		virtual ~CheckingAccount();
+
+		//
+		//
+
+	private:
+		long long mMinBalance;
+		long long mMinBalanceFee;
+
+		// apply min balance fee
+
+};
