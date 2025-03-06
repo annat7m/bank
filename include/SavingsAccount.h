0067843199ca2b72fca7fbd02ac047c7a2a0b103
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-03-06 23:43:56 +0000

    added all the headers

--- include/SavingsAccount.h
+++ include/SavingsAccount.h
@@ -6,3 +6,26 @@
 // Assignment:  Assignment 3 - Bank Accounts
 // Purpose:     Practice Object Oriented Design Skills
 //***************************************************************************
+
+#include <vector>
+#include "../include/Account.h"
+
+#pragma once
+
+class SavingAccount : public Account {
+
+	public:
+		SavingAccount (int accNumber, long long accBalance, double accInterestRate,
+			long long minBalance, long long monthlyFee);
+		virtual ~SavingAccount();
+
+		virtual void deposit (long long amount);
+		virtual void withdraw (long long amount);
+		virtual void chargeMonthlyFee ();
+		virtual void accrueInterest ();
+
+	private:
+		long long mMinBalance;
+		long long mMonthlyFee;
+
+};
