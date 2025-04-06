bd8d0351630d26bbd1674663b910fd020a062196
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-04-06 04:54:45 +0000

    added new headers

---
+++ include/CurrencyMismatchException.h
@@ -0,0 +1,31 @@
+//***************************************************************************
+// File name:   CurrencyMismatchException.h
+// Author:      Anna Tymoshenko
+// Date:        04/05/2025
+// Class:       CS485
+// Assignment:  Assignment 5 - Bank 3
+// Purpose:     Practice Object Oriented Design Skills
+//***************************************************************************
+
+#include <iostream>
+#include <string>
+#include "../include/Currency.h"
+
+#pragma once
+
+class CurrencyMismatchException : public std::exception {
+	public:
+	CurrencyMismatchException (Currency currency1, Currency currency2);
+	CurrencyMismatchException (const CurrencyMismatchException& rcOther);
+	~CurrencyMismatchException ();
+
+	CurrencyMismatchException& operator= (CurrencyMismatchException cOther);
+
+	virtual const char* what () const noexcept override;
+
+	private:
+	Currency mCurrency1;
+	Currency mCurrency2;
+	char* mpszMessage;
+
+};
