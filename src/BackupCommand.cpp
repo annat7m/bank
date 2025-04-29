3d32288c6fa0fc415f3ca82b91637531fb37b3ff
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-04-29 07:09:41 +0000

    added backup command, need to implement visitor pattern its using

---
+++ src/BackupCommand.cpp
@@ -0,0 +1,60 @@
+//***************************************************************************
+// File name:   BackupCommand.cpp
+// Author:      Anna Tymoshenko
+// Date:        04/28/2025
+// Class:       CS485
+// Assignment:  Bank 4
+// Purpose:     Practice Object Oriented Design Skills
+//***************************************************************************
+
+#include "../include/BackupCommand.h"
+#include "../include/Bank.h"
+
+#include <iostream>
+#include <memory>
+
+//***************************************************************************
+// Constructor: BackupCommand
+//
+// Description: Initializes BackupCommand with Bank pointer and two streams
+//
+// Parameters:  cpBank    		- pointer to the Bank object
+//              rcOutStream_S	- reference to the output stream for savings
+//															accounts
+//							rcOutStream_C	- reference to the output stream for checking
+//															accounts
+//
+// Returned:    None
+//***************************************************************************
+
+BackupCommand::BackupCommand (std::shared_ptr<Bank> cpBank, 
+	std::ostream& rcOutStream_S, std::ostream& rcOutStream_C) 
+	: mpcBank (cpBank), mrcOutStream_S (rcOutStream_S), 
+	mrcOutStream_C (rcOutStream_C) {}
+
+//***************************************************************************
+// Destructor:  PrintCommand
+//
+// Description: Default destructor
+//
+// Parameters:  None
+//
+// Returned:    None
+//***************************************************************************
+
+BackupCommand::~BackupCommand () {}
+
+//***************************************************************************
+// Function:    execute
+//
+// Description: Prints all savings accounts to the provided output stream
+//							and all cheecking accounts to a different output stream
+//
+// Parameters:  none
+//
+// Returned:    none
+//***************************************************************************
+
+void BackupCommand::execute () {
+	// use visitor pattern
+}
