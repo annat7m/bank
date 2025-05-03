4a19385ba3569ad93d279ea2a05583602f841816
tymo7256 <112516174+tymo7256@users.noreply.github.com>
2025-05-03 01:19:20 +0000

    added transfer command

---
+++ src/TransferCommand.cpp
@@ -0,0 +1,68 @@
+//***************************************************************************
+// File name:   TransferCommand.cpp
+// Author:      Anna Tymoshenko
+// Date:        04/26/2025
+// Class:       CS485
+// Assignment:  Bank 4
+// Purpose:     Practice Object Oriented Design Skills
+//***************************************************************************
+
+#include "../include/TransferCommand.h"
+#include "../include/Bank.h"
+#include "../include/CurrencyMismatchException.h"
+
+#include <iostream>
+#include <memory>
+
+//***************************************************************************
+// Constructor: TransferCommand
+//
+// Description: Initializes TransferCommand with Bank pointer, 2 account numbers, 
+//							and amount
+//
+// Parameters:  cpBank        		- pointer to the Bank object
+//              fromAccountNumber - account number to withdraw from
+//							toAccountNumber		- account number to deposit to
+//              cAmount       		- amount of money to transfer
+//
+// Returned:    None
+//***************************************************************************
+
+TransferCommand::TransferCommand (std::shared_ptr<Bank> cpBank,
+	unsigned int fromAccountNumber, unsigned int toAccountNumber, 
+	Money cAmount) : mpcBank (cpBank), mFromAccountNumber (fromAccountNumber), 
+	mToAccountNumber(toAccountNumber), mcAmount (cAmount) {
+}
+
+//***************************************************************************
+// Destructor:  TransferCommand
+//
+// Description: Default destructor
+//
+// Parameters:  None
+//
+// Returned:    None
+//***************************************************************************
+
+TransferCommand::~TransferCommand () {}
+
+//***************************************************************************
+// Function:    execute
+//
+// Description: Withdraws the specified amount of money from the given account
+//
+// Parameters:  none
+//
+// Returned:    none
+//***************************************************************************
+
+void TransferCommand::execute () {
+	// const std::string COMMAND = "W";
+	// try {
+	// 	mpcBank->withdraw (mAccountNumber, mcAmount);
+	// }
+	// catch (CurrencyMismatchException& e) {
+	// 	mpcBank->logCurrencyException (COMMAND, mAccountNumber, e.getFromCurrency (),
+	// 		e.getToCurrency ());
+	// }
+}
